77a7c7c0c255eb588b7cdf5b5b0fc724f7b6ea15
yummysushiii <89094866+yummysushiii@users.noreply.github.com>
2022-05-25 14:48:25 +0800

    Add files via upload

---
+++ hw7-1/hw7-1/src/account.js
@@ -0,0 +1,12 @@
+const changeText = (event) => {
+    console.log(event.target)
+    event.target.innerText = event.target.innerText + "被點了"
+}
+
+const account=(num)=>{ 
+    var output = <input type = "text"></input>;
+
+    return output;
+}
+  
+export default account;
